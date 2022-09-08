dfe0363b8516c97cc3e8e783cd50b36a744825f4
shubham maithani <shubhammaithani1@gmail.com>
2022-09-08 10:31:45 -0700

    check two strings equal or not

---
+++ comparestrings.java
@@ -0,0 +1,16 @@
+package com.practice.day1;
+
+public class comparestrings {
+
+	public static void main(String[] args) {
+		
+		String style1 = new String("shubham");
+        String style2 = new String("shubham");
+
+        if(style1 == style2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+	}
+
+}
